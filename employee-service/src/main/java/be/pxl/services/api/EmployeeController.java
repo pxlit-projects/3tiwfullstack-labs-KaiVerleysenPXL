583190e76e59bdd72784d8323d99d497eb8219b8
KaiVerleysenPXL <kai.verleysen@student.pxl.be>
2025-11-25 11:33:10 +0100

    add logging with slf4j

--- employee-service/src/main/java/be/pxl/services/api/EmployeeController.java
+++ employee-service/src/main/java/be/pxl/services/api/EmployeeController.java
@@ -4,6 +4,9 @@ import be.pxl.services.domain.dto.EmployeeRequest;
 import be.pxl.services.domain.dto.EmployeeResponse;
 import be.pxl.services.services.IEmployeeService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,11 +17,13 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/employee")
 @RequiredArgsConstructor
+@Slf4j
 public class EmployeeController {
     private final IEmployeeService employeeService;
 
     @GetMapping
     public ResponseEntity<List<EmployeeResponse>> getEmployee() {
+        log.info("logging this");
         return new ResponseEntity<>(employeeService.getAllEmployees(), HttpStatus.OK);
     }
 
